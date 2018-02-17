2b3805f6767263e1f3ddda77d03b0d990053860e
黄巍 <huan1043269994@gmail.com>
2018-02-17 11:12:11 +0800

    [ADD] add some code.

---
+++ 2/hello_world.py
@@ -0,0 +1,9 @@
+message = "Hello Python Crash Course reader!"
+print(message)
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+favorite_language = 'python '
+favorite_language.rstrip()
